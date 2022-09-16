ad515f3cc88c7d675803f3088e2f7f828bce5586
rostikn <rostanovak03@seznam.cz>
2022-09-16 09:23:26 +0200

    added depe

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("pepe");    }
+        System.out.println("pepe");
+        System.out.println("depe");
+    }
+
 }
